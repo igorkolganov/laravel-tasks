56483b3da3e80b3da93c4a149302e256505e1087
Igor Kolganov <igor3igor@rambler.ru>
2020-11-15 04:50:27 -0800

    remove some comments

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -17,11 +17,6 @@ class TasksController extends Controller
      */
     public function index()
     {
-        //$userId = auth('api')->id();
-        //$userId = Auth::check() ? Auth::id() : true;
-        //return ModelTask::all()->where('user_id', $userId);
-        //return DB::table('tasks')->get();
-
         $userId = DB::select('select id from users where authorized = :bol', ['bol' => true]);
         return DB::select('select * from tasks where user_id = :id', ['id' => $userId['0']->id]);
     }
@@ -55,11 +50,6 @@ class TasksController extends Controller
         $task->done = $request->input('done', false);
         $task->changed = $request->input('changed', false);
 
-//        $task->save();
-//        if(!!$task->save()){
-//            return new TaskResource($task);
-//        }
-
         DB::insert('insert into tasks (user_id, title, body, priority, done, changed) values (:user_id, :title, :body, :priority, :done, :changed)', ['user_id' => $task->user_id, 'title' => $task->title, 'body' => $task->body, 'priority' => $task->priority, 'done' => $task->done, 'changed' => $task->changed] );
     }
 
@@ -71,8 +61,6 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-        //return ModelTask::find($id);
-
         return DB::select('select * from tasks where id = :id', ['id' => $id]);
     }
 
@@ -96,31 +84,12 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-//        $task = ModelTask::find($id);
-//        $task->update($request->all());
-//        return $task;
-
-//        $task = [
-//            'title' => $request->input('title'),
-//            'body' => $request->input('body'),
-//            'done' => $request->input('done'),
-//            'priority' => $request->input('priority'),
-//            'changed' => $request->input('changed', true)
-//        ];
-
         $task = new \stdClass();
         $task->title = $request->input('title');
         $task->body = $request->input('body');
         $task->done = $request->input('done');
         $task->priority = $request->input('priority');
         $task->changed = $request->input('changed', true);
-
-        //return $task->title;
-
-        //return DB::update('update tasks set title = :title, body = :body, done = :done, priority = :priority, changed = :changed, where id = :id', [':title' => $task->title, ':body' => $task->body, ':done' => $task->done, ':priority' => $task->priority, ':changed' => $task->changed, ':id' => $id]);
-        //return DB::update('update tasks set title = :title, body = :body, done = :done, priority = :priority, changed = :changed where id = :id', ['title' => $task->title, 'body' => $task->body, 'done' => $task->done, 'priority' => $task->priority, 'changed' => $task->changed, 'id' => $id]);
-        //return DB::update('update tasks set done = :done  where id = :id', ['done' => $task->done, 'id' => $id]);
-        //tasks (user_id, title, body, priority, done, changed) values (:user_id, :title, :body, :priority, :done, :changed)
 
         if ($task->done !== null){
             return DB::update('update tasks set done = :done  where id = :id', ['done' => $task->done, 'id' => $id]);
@@ -137,8 +106,6 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //return ModelTask::destroy($id);
-
         return DB::delete('delete from tasks where id = :id', ['id' => $id]);
     }
 }
